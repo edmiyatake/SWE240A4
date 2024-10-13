412b361617256f2bba64c5484eafa03c657a6ee1
Edwin Miyatake <122956602+edmiyatake@users.noreply.github.com>
2024-10-12 22:31:53 -0700

    god forbid I found a solution

--- Assignment4.py
+++ Assignment4.py
@@ -1,5 +1,6 @@
 class Node:
     def __init__(self,str):
+        self.op = str[0]
         self.studentNum = str[slice(1,8)] # indices 1 -> 7 => 7 elements
         self.lastName = str[slice(8,33)] # indices 8 -> 32 => 25 elements
         self.home = str[slice(33,37)] # indices 33 -> 36 => 4 elements
@@ -11,10 +12,12 @@ class Node:
 class BST:
     def __init__(self):
         self.root = None
+        # self.size = 0 
+
     def choose(self,str):
-        if str[0] == "I":
+        if self.op == "I":
             self.insert(str)
-        elif str[0] == "D":
+        elif self.op == "D":
             self.delete(str)
         else:
             raise ValueError("Not a valid operation code")
@@ -23,25 +26,33 @@ class BST:
         # takes in string from file and inserts into tree
         # example input string: "I8599999Zot                      0451WET 1"
         # first check if root exists
-        newNode = Node(str)
         if not self.root:
-            self.root = newNode
-        else:
-            # do a comparison with parent node and make sure that the lowest asci goes to left
-            if ord(newNode.data[8]) < ord(self.root.data[8]):
-                self.root.left
+            self.root = Node(str)
+            return
+        currNode = self.root
+        while currNode:
+            if currNode.lastName > str[slice(8, 33)]:
+                if not currNode.left:
+                    currNode.left = Node(str)
+                    break
+                else:
+                    currNode = currNode.left
+            else:
+                if not currNode.right:
+                    currNode.right = Node(str)
+                    break
+                else:
+                    currNode = currNode.right
     
     def remove(self,str):
         exit
                 
+newBST = BST()
+f = open("tree-input.txt","r")
+for line in f:
+    newBST.insert(line)
+f.close()
 
-    
-
-
-# f = open("tree-input.txt","r")
-# # instead of trying to insert in the tree i think its better to sort and rewrite the input file
-# for line in f:
-#     print(line)
-
-# f.close()
-
+print(newBST.root.lastName)
+print(newBST.root.left.lastName)
+print(newBST.root.left.left.lastName)
