4580f786d86e27ebcf5e5ae70564d788f54479d2
Edwin Miyatake <edmiyatake@gmail.com>
2024-10-15 22:02:03 -0700

    maybe learned how to do a binary search tree?

--- treeTest.py
+++ treeTest.py
@@ -8,36 +8,43 @@ class BinaryTree:
     def __init__(self):
         self.root = None
 
-    def root(self):
+    def root(self,val):
+        self.root = Node(val)
         return self.root
-        
-    def insert(self,val):
-
-        if self.root == None:
-            self.root = Node(val)
-            return
-
-        currNode = self.root
-        while currNode:
-            if currNode.data > val:
-                # if currNode is empty, insert into left
-                if not currNode.left:
-                    currNode.left = Node(val)
-                    return
-                else:
-                    currNode = currNode.left
+    
+    def insert(self,root,node):
+        if root is None:
+            root = node
+        if root.data > node.data:
+            if root.left is None:
+                root.left = node
             else:
-                if not currNode.right:
-                    currNode.right = Node(val)
-                    return
-                else:
-                    currNode = currNode.right
+                self.insert(root.left,node)
+        else:
+            if root.right is None:
+                root.right = node
+            else:
+                self.insert(root.right,node)
 
     def inOrder(self,node):
         if node:
             self.inOrder(node.left)
             print(node.data)
             self.inOrder(node.right)
+
+    def in_order_print(self,root):
+        if not root:
+            return
+        self.in_order_print(root.left)
+        print(root.data)
+        self.in_order_print(root.right)
+
+    def pre_order_print(self,root):
+        if not root:
+            return
+        print(root.data)
+        self.in_order_print(root.left)
+        self.in_order_print(root.right)
     
     def preOrder(self,node):
         if node:
@@ -51,14 +58,27 @@ class BinaryTree:
             self.inOrder(node.right)
             print(node.data)
 
-test = [1,23,45,65,2,32]
+    def printTree(self,node, level=0):
+        if not node:
+            raise ValueError("EMPTY TREE")
+        if node != None:
+            self.printTree(node.left, level + 1)
+            print(' ' * 4 * level + '-> ' + str(node.data))
+            self.printTree(node.right, level + 1)
+
+
+test = [7,1,5]
 newBST = BinaryTree()
+root = Node(3)
 for num in test:
-    newBST.insert(num)
-print(newBST.root)
-# newBST.inOrder(newBST.root)
-newBST.preOrder(newBST.root)
+    newBST.insert(root, Node(num))
+# newBST.inOrder(root)
+# newBST.preOrder(newBST.root)
 # newBST.postOrder(newBST.root)
+# newBST.postOrder(newBST.root)
+# newBST.printTree(root)
+newBST.in_order_print(root)
+newBST.pre_order_print(root)
 
 
 
